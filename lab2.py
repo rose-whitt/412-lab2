21a16676027e49f52d4d0e63238a4bb9fe4a7ec3
rose-whitt <rew9@rice.edu>
2023-09-27 17:45:27 -0500

    reverse iter

--- lab2.py
+++ lab2.py
@@ -2,6 +2,11 @@ import scanner
 from IR_List import *
 import lab1
 import sys
+import math
+
+# MACROS
+INVALID = -1
+INF = math.inf
 
 class Lab2:
   def __init__(self):
@@ -12,23 +17,27 @@ class Lab2:
     self.VR_name = 0
     self.SR_to_VR = []
     self.LU = []
+    
 
    
+  def rename(self):
+    print("in rename")
 
-  def main():
-    print("iN LAB2 MOFO")
-    lab1.main(True, True)
-    print(lab1.ir_list)
-    print(lab1.ir_list.length)
+    self.SR_to_VR = [INVALID for i in range(self.max_sr_num)]
+    self.LU = [INF for i in range(self.max_sr_num)]
 
+    index = self.IR_LIST.length - 1
 
-  def rename():
-    max_sr_num = lab1.ir_list.length * 3
-    # for i in range(max_sr_num):
+    OP = self.IR_LIST.tail
+    while (OP != None):  # For each OPCODE (OP) in the block, bottom to top
+      print(OP)
+      OP = OP.prev
+    
+
 
 def main():
   print("in main")
   lab2 = Lab2()
-  lab2
+  lab2.rename()
 if __name__ == "__main__":
   main()
